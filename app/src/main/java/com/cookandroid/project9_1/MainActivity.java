5f29a7bd0653b19f64e7962381ee8480ff6f8de7
hawawaii <fnzkaps@naver.com>
2025-04-13 22:10:34 +0900

    131010

--- app/src/main/java/com/cookandroid/project9_1/MainActivity.java
+++ app/src/main/java/com/cookandroid/project9_1/MainActivity.java
@@ -5,10 +5,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    final static int LINE = 1, CIRCLE = 2;
+    static int curShape = LINE;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
     }
 }
