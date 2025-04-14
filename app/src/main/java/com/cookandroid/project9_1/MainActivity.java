79d37fcda87e24bd74c3b22f9daab0b5b089472e
hawawaii <fnzkaps@naver.com>
2025-04-14 19:23:11 +0900

    141923

--- app/src/main/java/com/cookandroid/project9_1/MainActivity.java
+++ app/src/main/java/com/cookandroid/project9_1/MainActivity.java
@@ -1,7 +1,11 @@
 package com.cookandroid.project9_1;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.MotionEvent;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +15,55 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(new MyGraphicView(this));
+        setTitle("간단한 그림판");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        menu.add(0, 1, 0, "선 그리기");
+        menu.add(0, 2, 0, "원 그리기");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case 1:
+                curShape = LINE;
+                return true;
+            case 2:
+                curShape = CIRCLE;
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private static class MyGraphicView extends View {
+        int startX = -1, startY = -1, stopX = -1, stopY = -1;
+
+        public MyGraphicView(Context context) {
+            super(context);
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent event) {
+            switch (event.getAction()) {
+                case MotionEvent.ACTION_DOWN:
+                    startX = (int) event.getX();
+                    startY = (int) event.getY();
+                    break;
+                case MotionEvent.ACTION_MOVE:
+                case MotionEvent.ACTION_UP:
+                    stopX = (int) event.getX();
+                    stopY = (int) event.getY();
+                    this.invalidate();
+                    break;
+            }
+            return true;
+        }
+
+
     }
 }
